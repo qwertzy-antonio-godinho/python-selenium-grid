5df53766f2168c289409ad8424a7d26636d120cb
qwertzy-antonio-godinho <qwertzy-antonio-godinho@gmail.com>
2022-07-17 00:03:40 +0100

    Implemented explicit waits with expected conditions

--- src/pageobject/home.py
+++ src/pageobject/home.py
@@ -4,6 +4,9 @@ Page Object Model definition for Home page.
 from dataclasses import dataclass
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 @dataclass
@@ -12,6 +15,7 @@ class PageLocators:
 
     element_search_textbox = "s"
     element_search_button = "search-image"
+    element_topics_dropdown = "cat"
 
 
 class PageObjects:
@@ -22,11 +26,24 @@ class PageObjects:
         Searches for elements in the page."""
         self.driver = driver
 
-        self.element_search_textbox = driver.find_element(
-            By.ID, PageLocators.element_search_textbox
+        self.element_search_textbox = WebDriverWait(driver, 10).until(
+            expected_conditions.presence_of_element_located(
+                (By.ID, PageLocators.element_search_textbox)
+            )
         )
-        self.element_search_button = driver.find_element(
-            By.ID, PageLocators.element_search_button
+
+        self.element_search_button = WebDriverWait(driver, 10).until(
+            expected_conditions.presence_of_element_located(
+                (By.ID, PageLocators.element_search_button)
+            )
+        )
+
+        self.element_topics_dropdown_selection = Select(
+            WebDriverWait(driver, 10).until(
+                expected_conditions.presence_of_element_located(
+                    (By.ID, PageLocators.element_topics_dropdown)
+                )
+            )
         )
 
     def get_search_textbox(self):
@@ -36,3 +53,7 @@ class PageObjects:
     def get_search_button(self):
         """Method to access and usage of element in the tests."""
         return self.element_search_button
+
+    def get_topics_dropdown(self):
+        """Method to access and usage of element in the tests."""
+        return self.element_topics_dropdown_selection
